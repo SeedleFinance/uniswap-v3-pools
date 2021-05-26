6d3d4a5f4cdf41f3a152540d20919cee1ec958f5
Lakshan Perera <lakshan@laktek.com>
2021-05-26 17:57:53 +1000

    added footer

--- src/Container.tsx
+++ src/Container.tsx
@@ -42,6 +42,21 @@ function Container() {
           <div>
             <Pools pools={pools} />
           </div>
+          <footer className="my-5 flex w-full justify-center">
+            <div className="text-sm">
+              Built by{" "}
+              <a className="text-blue-500" href="https://twitter.com/laktek">
+                @laktek
+              </a>{" "}
+              |{" "}
+              <a
+                className="text-blue-500"
+                href="https://github.com/laktek/uniswap-v3-pools"
+              >
+                Source
+              </a>
+            </div>
+          </footer>
         </div>
       </div>
     );
