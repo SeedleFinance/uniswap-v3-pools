7d252d02a25f7827283c08cec1d77e1b13c30f33
Lakshan Perera <lakshan@laktek.com>
2021-06-08 20:10:10 +1000

    tweaked the range visual

--- src/RangeVisual.tsx
+++ src/RangeVisual.tsx
@@ -15,43 +15,40 @@ function RangeVisual({
   tickSpacing,
   flip,
 }: RangeVisualProps) {
-  let [minTick, maxTick] = [tickLower, tickUpper].sort((a, b) =>
-    flip ? a - b : b - a
-  );
-
-  const spacing = tickSpacing / 100;
-  let spaceMultiplier = 2;
+  const spacing = tickSpacing * 100;
+  let spaceMultiplier = 10;
   if (tickSpacing === 200) {
-    spaceMultiplier = 1;
+    spaceMultiplier = 10;
   } else if (tickSpacing === 10) {
-    spaceMultiplier = 5;
+    spaceMultiplier = 2;
   }
-  const rangeStart = tickCurrent - spacing * spaceMultiplier;
-  const rangeEnd = tickCurrent + spacing * spaceMultiplier;
+  let rangeStart = tickCurrent - spacing * spaceMultiplier;
+  let rangeEnd = tickCurrent + spacing * spaceMultiplier;
+
+  if (rangeStart > rangeEnd || flip) {
+    [rangeStart, rangeEnd] = [rangeEnd, rangeStart];
+  }
 
   const barWidth = 250;
   const scale = barWidth / (rangeEnd - rangeStart);
-  const beforeRangeWidth = Math.abs((minTick - rangeStart) * scale);
-  const rangeWidth = Math.abs((maxTick - minTick) * scale);
-  const afterRangeWidth = Math.abs((rangeEnd - maxTick) * scale);
-  const currentTickDistance = Math.min(
-    Math.abs((tickCurrent - rangeStart) * scale),
-    barWidth
-  );
+  const beforeRangeWidth = Math.abs((tickLower - rangeStart) * scale);
+  const rangeWidth = Math.abs((tickUpper - tickLower) * scale);
+  const afterRangeWidth = Math.abs((rangeEnd - tickUpper) * scale);
+  const currentTickDistance = Math.abs((tickCurrent - rangeStart) * scale);
 
   return (
     <div
-      className="flex border rounded"
+      className="flex border rounded bg-gray-200"
       style={{ position: "relative", width: `${barWidth}px`, height: "16px" }}
     >
       <div
-        className="bg-gray-200"
-        style={{ width: `${beforeRangeWidth}px` }}
-      ></div>
-      <div className="bg-blue-400" style={{ width: `${rangeWidth}px` }}></div>
-      <div
-        className="bg-gray-200"
-        style={{ width: `${afterRangeWidth}px` }}
+        className="bg-blue-400"
+        style={{
+          position: "absolute",
+          height: "14px",
+          left: `${beforeRangeWidth}px`,
+          width: `${rangeWidth.toFixed(2)}px`,
+        }}
       ></div>
       <div
         className="bg-red-400"
@@ -59,7 +56,7 @@ function RangeVisual({
           width: "2px",
           height: "14px",
           position: "absolute",
-          left: `${currentTickDistance}px`,
+          left: `${currentTickDistance.toFixed(2)}px`,
         }}
       ></div>
     </div>
