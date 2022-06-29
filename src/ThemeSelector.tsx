ed336c260d8c17331f6fcdaf90ed1bfcaa3c8f23
Elliott <elliott@elliottrisby.com>
2022-06-29 15:05:22 +0930

    simplify theme selector

--- src/ThemeSelector.tsx
+++ src/ThemeSelector.tsx
@@ -3,11 +3,12 @@ import { faCaretDown, faSun, faMoon, faDesktop } from '@fortawesome/free-solid-s
 
 import { useAppSettings } from './AppSettingsProvider';
 import Icon from './ui/Icon/Icon';
-import Menu from './ui/Menu/Menu';
+import SunIcon from './icons/Sun';
+import MoonIcon from './icons/Moon';
+import { Button } from './ui/Button';
 
 function ThemeSelector() {
   const { theme, setTheme } = useAppSettings();
-  const [selectorExpanded, setSelectorExpanded] = useState(false);
 
   const computedTheme = useMemo(() => {
     if (theme === '') {
@@ -16,46 +17,15 @@ function ThemeSelector() {
     return theme;
   }, [theme]);
 
-  const handleSelection = (val: string) => {
-    setTheme(val);
-    setSelectorExpanded(false);
-  };
+  function handleClickToggleTheme() {
+    setTheme((prevState: string) => (prevState === 'dark' ? 'light' : 'dark'));
+  }
 
   return (
-    <div className="p-3 mx-1 md:mx-2 rounded-md border relative border-element-10 text-high flex flex-shrink-0">
-      <button
-        className="flex items-center focus:outline-none"
-        onClick={() => setSelectorExpanded(!selectorExpanded)}
-      >
-        <Icon className="text-xl" size="sm" icon={computedTheme === 'light' ? faSun : faMoon} />
-        <Icon className="pl-1 text-xl" icon={faCaretDown} />
-      </button>
-
-      {selectorExpanded && (
-        <Menu onClose={() => setSelectorExpanded(false)} className="top-12 left-0 w-32 shadow-lg">
-          <button
-            className="flex items-center justify-between w-24 my-1"
-            onClick={() => handleSelection('light')}
-          >
-            <Icon size="sm" className="w-1/5 mr-2" icon={faSun} />
-            <span className="w-4/5 text-left">Light</span>
-          </button>
-          <button
-            className="flex items-center justify-between w-24 my-1"
-            onClick={() => handleSelection('dark')}
-          >
-            <Icon size="sm" className="w-1/5 mr-2" icon={faMoon} />
-            <span className="w-4/5 text-left">Dark</span>
-          </button>
-          <button
-            className="flex items-center justify-between w-24 my-1"
-            onClick={() => handleSelection('')}
-          >
-            <Icon size="sm" className="w-1/5 mr-2" icon={faDesktop} />
-            <span className="w-4/5 text-left">System</span>
-          </button>
-        </Menu>
-      )}
+    <div className="border border-element-10 p-0 rounded-md mr-1">
+      <Button onClick={handleClickToggleTheme} variant="ghost">
+        {computedTheme === 'light' ? <SunIcon /> : <MoonIcon />}
+      </Button>
     </div>
   );
 }
