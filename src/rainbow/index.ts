0b6b2d0565d675f73fc350c90ec69f9f6631a9cb
Elliott <elliott@elliottrisby.com>
2022-08-01 21:41:54 +0930

    update wallet connectors

--- src/rainbow/index.ts
+++ src/rainbow/index.ts
@@ -1,5 +1,6 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient } from 'wagmi';
+import { connectorsForWallets, wallet } from '@rainbow-me/rainbowkit';
 
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
@@ -10,10 +11,18 @@ export const { chains, provider } = configureChains(
   [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()],
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'Seedle Finance',
-  chains,
-});
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Recommended',
+    wallets: [
+      wallet.metaMask({ chains }),
+      wallet.ledger({ chains }),
+      wallet.coinbase({ appName: 'Seedle Finance', chains }),
+      wallet.rainbow({ chains }),
+      wallet.walletConnect({ chains }),
+    ],
+  },
+]);
 
 export const wagmiClient = createClient({
   autoConnect: true,
