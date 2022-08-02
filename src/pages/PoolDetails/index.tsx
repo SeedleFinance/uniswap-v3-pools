9dafad41d177416a2690df7a4242d7c281c75f1f
Lakshan Perera <lakshan@laktek.com>
2022-08-03 03:48:39 +1000

    fix pool loading state

--- src/pages/PoolDetails/index.tsx
+++ src/pages/PoolDetails/index.tsx
@@ -45,6 +45,21 @@ const PoolDetailsPage = () => {
     return price;
   }, [pool]);
 
+  if (!pool?.positions) {
+    return (
+      <div>
+        <div className="flex items-center">
+          <div className="flex flex-col">
+            <div className="bg-surface-10 rounded w-32 h-4"></div>
+            <div className="bg-surface-10 rounded-sm w-96 h-12 mt-4"></div>
+          </div>
+        </div>
+        <div className="bg-surface-10 rounded w-full h-20 mt-8"></div>
+        <div className="bg-surface-10 rounded w-full h-20 mt-4"></div>
+      </div>
+    );
+  }
+
   const {
     key,
     address,
@@ -63,21 +78,6 @@ const PoolDetailsPage = () => {
   }
 
   function handleClickDownloadCSV() {}
-
-  if (!pool?.positions) {
-    return (
-      <div>
-        <div className="flex items-center">
-          <div className="flex flex-col">
-            <div className="bg-surface-10 rounded w-32 h-4"></div>
-            <div className="bg-surface-10 rounded-sm w-96 h-12 mt-4"></div>
-          </div>
-        </div>
-        <div className="bg-surface-10 rounded w-full h-20 mt-8"></div>
-        <div className="bg-surface-10 rounded w-full h-20 mt-4"></div>
-      </div>
-    );
-  }
 
   return (
     <div className="flex flex-col w-full">
