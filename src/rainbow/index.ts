243e09f8696b1331e2d47c3b477bf943e9e29f7a
Lakshan Perera <lakshan@laktek.com>
2022-08-09 17:43:32 +1000

    Updated JsonRpc provider with URLs

--- src/rainbow/index.ts
+++ src/rainbow/index.ts
@@ -1,15 +1,28 @@
 import { chain, configureChains, createClient } from 'wagmi';
 import { connectorsForWallets, wallet } from '@rainbow-me/rainbowkit';
 
-import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 
 export const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
   //@ts-ignore
   [
-    alchemyProvider({ apiKey: 'saJ_d7L6OvoZ3t6jL7ewhv7ONWWi_J29', priority: 0 }),
-    publicProvider({ priority: 1 }),
+    jsonRpcProvider({
+      rpc: (chain) => {
+        const urls = {
+          1: 'https://eth-mainnet.alchemyapi.io/v2/saJ_d7L6OvoZ3t6jL7ewhv7ONWWi_J29',
+          10: 'https://opt-mainnet.g.alchemy.com/v2/bEw3og1rC9BHAidSjH24d18OEPFnEyCC',
+          42161: 'https://arb-mainnet.g.alchemy.com/v2/cduelMqriBVheVg-8kGl_1ORm9NEK6ek',
+          137: 'https://polygon-mainnet.g.alchemy.com/v2/Fjjq6qMa4rJR-U4jk9KdyoAwY0pdR9KL',
+        };
+
+        return {
+          http: urls[chain.id],
+        };
+      },
+    }),
+    publicProvider(),
   ],
 );
 
