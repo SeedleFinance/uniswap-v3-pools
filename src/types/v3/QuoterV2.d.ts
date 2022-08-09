df4150f5e1c665b84c3f3ab05a7c602b6d19c750
Lakshan Perera <lakshan@laktek.com>
2022-08-10 00:37:12 +1000

    removed v3-sdk version resolution

--- src/types/v3/QuoterV2.d.ts
+++ src/types/v3/QuoterV2.d.ts
@@ -17,16 +17,16 @@ import {
 import { BytesLike } from "@ethersproject/bytes";
 import { Listener, Provider } from "@ethersproject/providers";
 import { FunctionFragment, EventFragment, Result } from "@ethersproject/abi";
-import { TypedEventFilter, TypedEvent, TypedListener } from "./commons";
+import type { TypedEventFilter, TypedEvent, TypedListener } from "./common";
 
 interface QuoterV2Interface extends ethers.utils.Interface {
   functions: {
     "WETH9()": FunctionFragment;
     "factory()": FunctionFragment;
     "quoteExactInput(bytes,uint256)": FunctionFragment;
-    "quoteExactInputSingle(tuple)": FunctionFragment;
+    "quoteExactInputSingle((address,address,uint256,uint24,uint160))": FunctionFragment;
     "quoteExactOutput(bytes,uint256)": FunctionFragment;
-    "quoteExactOutputSingle(tuple)": FunctionFragment;
+    "quoteExactOutputSingle((address,address,uint256,uint24,uint160))": FunctionFragment;
     "uniswapV3SwapCallback(int256,int256,bytes)": FunctionFragment;
   };
 
