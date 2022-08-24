13847e4bee73ba549873de14cb67d01aa0c7f118
Lakshan Perera <lakshan@laktek.com>
2022-08-24 15:48:06 +1000

    fix tokens page

--- src/pages/Tokens/index.tsx
+++ src/pages/Tokens/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import { useTokens } from '../../hooks/useTokens';
 import BackArrow from '../../icons/LeftArrow';
 import LoadingSpinner from '../../ui/Spinner';
+import { getChainNameAndColor } from '../../utils/chains';
+import { useCurrencyConversions } from '../../CurrencyConversionsProvider';
 
 const TokensPage = () => {
   const navigate = useNavigate();
-  const { data, loading, error } = useTokens();
+  const { convertToGlobalFormatted } = useCurrencyConversions();
+  const { loading, tokens } = useTokens(1);
 
   function handleClickBack() {
     navigate(-1);
@@ -20,15 +24,7 @@ const TokensPage = () => {
     );
   }
 
-  if (error) {
-    return (
-      <div className="w-full h-full flex items-center justify-center text-low">
-        Unable to load tokens.
-      </div>
-    );
-  }
-
-  if (!data) {
+  if (!tokens) {
     return <div>You do not have any tokens.</div>;
   }
 
@@ -48,23 +44,31 @@ const TokensPage = () => {
             <th className="md:px-6 py-4 whitespace-nowrap font-medium text-left">Token</th>
             <th className="text-right px-6 py-4 whitespace-nowrap font-medium">Balance</th>
             <th className="text-right px-6 py-4 whitespace-nowrap font-medium">Current Price</th>
+            <th className="text-right px-6 py-4 whitespace-nowrap font-medium">Value</th>
           </tr>
         </thead>
         <tbody className="text-0.875">
-          {data.map((token) => (
+          {tokens.map((token) => (
             <tr key={token.name} className="w-full hover:bg-surface-10 cursor-pointer">
               <td className="md:px-6 py-4 whitespace-nowrap flex items-center font-medium text-left">
                 <img className="w-8 h-8 mr-2" alt={`${token.name} logo`} src={token.logo} />
                 {token.name}
-                <div className="text-0.75 px-1 py-0.5 bg-slate-200 rounded-md ml-2 font-medium text-black">
-                  {token.network}
+                <div
+                  className={`text-0.75 px-1 py-0.5 rounded-md ml-1 font-medium text-black ${
+                    getChainNameAndColor(token.chainId)[1]
+                  }`}
+                >
+                  {getChainNameAndColor(token.chainId)[0]}
                 </div>
               </td>
               <td className="text-right px-6 py-4 whitespace-nowrap font-medium">
                 {token.balance}
               </td>
               <td className="text-right px-6 py-4 whitespace-nowrap font-medium">
-                ${token.price.toFixed(5)}
+                {convertToGlobalFormatted(token.price)}
+              </td>
+              <td className="text-right px-6 py-4 whitespace-nowrap font-medium">
+                {convertToGlobalFormatted(token.value)}
               </td>
             </tr>
           ))}
