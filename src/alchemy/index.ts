54c1c01af4de00ad7da4b8c267ceac2544b171a2
Lakshan Perera <lakshan@laktek.com>
2022-08-24 11:18:55 +1000

    removed alchemy

--- src/alchemy/index.ts
+++
@@ -1,8 +0,0 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-
-export function getAlchemy(network: Network): Alchemy {
-  return new Alchemy({
-    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-    network,
-  });
-}
