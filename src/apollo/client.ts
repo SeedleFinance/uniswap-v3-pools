a58deeb7a65b080f4bd3e29557784d321c894e3d
Lakshan Perera <lakshan@laktek.com>
2022-09-02 11:46:37 +1000

    fix URL

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const mainnetClient = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3-mainnet',
+  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
   cache: new InMemoryCache(),
   queryDeduplication: false,
   defaultOptions: {
