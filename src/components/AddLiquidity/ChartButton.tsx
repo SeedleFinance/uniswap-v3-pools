0a5bc90e00f193c1324423d2a8a7016f1adc8cc7
Elliott <elliott@elliottrisby.com>
2022-09-14 08:45:06 +0930

    fix up chart icon

--- src/components/AddLiquidity/ChartButton.tsx
+++ src/components/AddLiquidity/ChartButton.tsx
@@ -9,12 +9,25 @@ interface Props {
 function ChartButton({ selected, onClick }: Props) {
   return (
     <button
-      className={`border rounded-full px-2 py-1 dark:bg-slate-700 ${
+      className={`flex flex-shrink-0 rounded-full p-2 dark:bg-slate-700 ${
         selected ? 'bg-blue-200 dark:bg-slate-500' : 'bg-slate-200 dark:bg-slate-700'
       }`}
       onClick={onClick}
     >
-      {/* <Icon className="text-lg" size="sm" icon={faChartLine} /> */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-5 h-5"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+        />
+      </svg>
     </button>
   );
 }
