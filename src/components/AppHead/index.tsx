00d204255f606ed3fa4017e7e3334ad0b3e675ee
Elliott <elliott@elliottrisby.com>
2022-09-19 10:28:01 +0930

    update favicons
    cont

--- src/components/AppHead/index.tsx
+++ src/components/AppHead/index.tsx
@@ -5,9 +5,9 @@ const AppHead = () => {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1.0" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon32.png" />
+      <link rel="icon" type="image/png" sizes="64x64" href="/favicon64.png" />
+      <link rel="icon" type="image/png" sizes="128x128" href="/favicon128.png" />
       <link rel="manifest" href="/site.webmanifest" />
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
