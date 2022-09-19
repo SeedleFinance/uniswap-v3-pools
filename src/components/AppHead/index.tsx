cdd5e693c4ad4d4c910f0fc3cf5a192a0e1b8953
Elliott <elliott@elliottrisby.com>
2022-09-19 13:02:54 +0930

    update share image

--- src/components/AppHead/index.tsx
+++ src/components/AppHead/index.tsx
@@ -8,6 +8,11 @@ const AppHead = () => {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon32.png" />
       <link rel="icon" type="image/png" sizes="64x64" href="/favicon64.png" />
       <link rel="icon" type="image/png" sizes="128x128" href="/favicon128.png" />
+      <meta name="description" content="" />
+      <meta property="og:title" content="Seedle Finance" />
+      <meta property="og:description" content="Manage Uniswap Positions" />
+      <meta property="og:image" content="/seedle.png" />
+      <meta property="og:url" content="https://seedle.finance" />
       <link rel="manifest" href="/site.webmanifest" />
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
