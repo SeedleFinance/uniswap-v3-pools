bfd22e90c3fdf93ffd8129c04e59add30cd46bc2
Elliott <elliott@elliottrisby.com>
2023-02-11 22:04:18 +1030

    updated rainbow wallet / wagmi

--- src/lib/rainbow/index.ts
+++ src/lib/rainbow/index.ts
@@ -1,21 +1,26 @@
-import { chain, configureChains, createClient } from "wagmi";
-import { connectorsForWallets, wallet } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+  metaMaskWallet,
+  ledgerWallet,
+  rainbowWallet,
+  walletConnectWallet,
+  coinbaseWallet,
+} from '@rainbow-me/rainbowkit/wallets';
 
-import { publicProvider } from "wagmi/providers/public";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-
-export const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
   [
     jsonRpcProvider({
       rpc: (chain) => {
         const urls: { [index: number]: string } = {
-          1: "https://eth-mainnet.alchemyapi.io/v2/saJ_d7L6OvoZ3t6jL7ewhv7ONWWi_J29",
-          10: "https://opt-mainnet.g.alchemy.com/v2/bEw3og1rC9BHAidSjH24d18OEPFnEyCC",
-          42161:
-            "https://arb-mainnet.g.alchemy.com/v2/cduelMqriBVheVg-8kGl_1ORm9NEK6ek",
-          137: "https://polygon-mainnet.g.alchemy.com/v2/Fjjq6qMa4rJR-U4jk9KdyoAwY0pdR9KL",
+          1: 'https://eth-mainnet.alchemyapi.io/v2/saJ_d7L6OvoZ3t6jL7ewhv7ONWWi_J29',
+          10: 'https://opt-mainnet.g.alchemy.com/v2/bEw3og1rC9BHAidSjH24d18OEPFnEyCC',
+          42161: 'https://arb-mainnet.g.alchemy.com/v2/cduelMqriBVheVg-8kGl_1ORm9NEK6ek',
+          137: 'https://polygon-mainnet.g.alchemy.com/v2/Fjjq6qMa4rJR-U4jk9KdyoAwY0pdR9KL',
         };
 
         return {
@@ -24,24 +29,28 @@ export const { chains, provider } = configureChains(
       },
     }),
     publicProvider(),
-  ]
+  ],
 );
+
+const appName = 'Seedle Finance';
 
 const connectors = connectorsForWallets([
   {
-    groupName: "Recommended",
+    groupName: 'Recommended',
     wallets: [
-      wallet.metaMask({ chains }),
-      wallet.ledger({ chains }),
-      wallet.coinbase({ appName: "Seedle Finance", chains }),
-      wallet.rainbow({ chains }),
-      wallet.walletConnect({ chains }),
+      metaMaskWallet({ chains }),
+      ledgerWallet({ chains }),
+      coinbaseWallet({ appName, chains }),
+      rainbowWallet({ chains }),
+      walletConnectWallet({ chains }),
     ],
   },
 ]);
 
-export const wagmiClient = createClient({
+const wagmiClient = createClient({
   autoConnect: true,
   connectors,
   provider,
 });
+
+export { wagmiClient, chains };
